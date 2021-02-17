e1270509ee02df2ab4cd4f391aa15d3d75cd1e06
kimuihyeon <57362030+kimuihyeon222@users.noreply.github.com>
2021-02-17 21:08:49 +0900

    Update 12764 싸지방에 간 준하.cpp

--- Baekjoon/Queue/12764 싸지방에 간 준하.cpp
+++ Baekjoon/Queue/12764 싸지방에 간 준하.cpp
@@ -7,48 +7,53 @@ int main(){
 	int n;
 	priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
 	priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > where; //컴퓨터의 자리를 나타내는 큐 
-	vector<int> computer(500000, 0); //컴퓨터 1~x번까지 사용횟수 저장 배열
-	int com=0; //현재 컴퓨터 위치를 나타내기 위한 변수
-	 
+	priority_queue<int, vector<int>, greater<int> > useful; //사용가능한 컴퓨터 확인(가장 작은 컴퓨터부터 쓸수있게 오름차순으로 정렬하게함)
+	vector<int> computer(500005, 0); //컴퓨터 1~x번까지 각 컴퓨터 사용횟수 저장 배열
+	
 	cin >> n;
 	for(int i=0; i<n; i++){
 		int start, end;
 		cin >> start >> end;
 		pq.push(make_pair(start, end));
-	}  
-	
-	if(pq.top().second == 0){ //끝나는 시간이 0이면 빼버림 
-		pq.pop();
+		useful.push(i);
 	}
-	int start=pq.top().first; //컴퓨터 시작 시간
-	where.push(make_pair(pq.top().second, com)); //끝나는 시간과 컴퓨터 위치를 queue에 삽입
-	computer[com]++; 
+	
+	//맨 
+	where.push(make_pair(pq.top().second, useful.top())); //끝나는 시간과 컴퓨터 위치를 queue에 삽입
+	computer[useful.top()]++;
 	pq.pop();
-	
+	useful.pop();
+	int seat=0; //컴퓨터 최대 몇개인지 나타내는 변수
+	 
 	while(!pq.empty()){
-		com++;
-		//구간안에 시작 값이 포함될 경우
-		if(start < pq.top().first && pq.top().first < where.top().first){
-			//시작위치를 바꿔줌
-			start = pq.top().first;
-		}
-		else{ //컴퓨터 하나가 끝나고 새로운 사람이 그 자리로 들어갈 경우 
-			start = pq.top().first;
-			computer[where.top().second]++; //끝난 자리에 +1를 해줌
-			where.push(make_pair(pq.top().second, where.top().second)); //끝나는 시간과 컴퓨터 위치를 queue에 삽입
-			where.pop();
+		//컴퓨터의 끝나는 시간이 시작시간보다 클 경우 queue에 삽입
+		if(pq.top().first < where.top().first){
+			computer[useful.top()]++;
+			if(useful.top() > seat){
+				seat = useful.top();
+			}
+			where.push(make_pair(pq.top().second, useful.top())); //컴퓨터를 한대 더 사용한 것이므로 해당위치와 끝위치를 저장, size만큼 저장하면 컴퓨터의 갯수를 알수있음 
 			pq.pop();
-			com--; //원래 사용하던 자리에 들어갔으니 com을 하나 줄여줌 
-			continue;
+			useful.pop();
 		}
-		where.push(make_pair(pq.top().second, com)); //끝나는 시간과 컴퓨터 위치를 queue에 삽입
-		computer[com]++; //해당위치를 하나 증가 
-		pq.pop(); //구간을 사용했으니 빼줌 
+		//시작시간이 끝나는 시간보다 클 경우 더 작은 것들을 queue에서 전부 뺌 -> 사용자가 컴퓨터 사용을 끝낸거임
+		else{
+			while(!where.empty()){
+				if(where.top().first < pq.top().first){
+					useful.push(where.top().second);
+					where.pop();
+				}
+				else break;
+			}
+			computer[useful.top()]++;
+			where.push(make_pair(pq.top().second, useful.top())); //컴퓨터를 한대 더 사용한 것이므로 해당위치와 끝위치를 저장, size만큼 저장하면 컴퓨터의 갯수를 알수있음 
+			pq.pop();
+			useful.pop();
+		}
 	}
-	
-	cout << com+1 <<"\n";
-	for(int i=0; i<=com; i++){
+	cout << seat+1 << "\n";
+	for(int i=0; i<seat+1; i++){
 		cout << computer[i] << " ";
-	} 
+	}
 	return 0;
 }
