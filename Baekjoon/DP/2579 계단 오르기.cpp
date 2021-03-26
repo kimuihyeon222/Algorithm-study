dea94ae3e30416962a9d1633cc7face18baea126
kimuihyeon <57362030+kimuihyeon222@users.noreply.github.com>
2021-03-26 22:10:44 +0900

    Update 2579 계단 오르기.cpp

--- Baekjoon/DP/2579 계단 오르기.cpp
+++ Baekjoon/DP/2579 계단 오르기.cpp
@@ -31,3 +31,30 @@ int main() {
 	cout << two[n];
 	return 0;
  }
+
+
+아래코드는 파이썬
+n = int(input())
+
+stair = [0]
+
+for _ in range(n):
+    stair.append(int(input()))
+
+step1 = [0 for _ in range(n+1)] # 연속 한 번일 경우
+step2 = [0 for _ in range(n+1)] # 연속 두 번일 경우
+
+step1[1], step2[1] = stair[1], stair[1]
+
+if n >= 2:
+    for i in range(2,n+1):
+        step1[i] = step1[i-2] + stair[i]
+        if i == 2:
+            step2[i] = stair[i] + stair[i-1]
+        else:
+            step2[i] = stair[i] + stair[i-1] + step2[i-3]
+        
+        #둘 중에 큰 값으로 둘다 초기화
+        step1[i], step2[i] = max(step1[i], step2[i]), max(step1[i], step2[i])
+
+print(step1[n])
