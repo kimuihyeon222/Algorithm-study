ed72f6f89f9414eb56ecd8855c4202ac263c6e23
kimuihyeon <57362030+kimuihyeon222@users.noreply.github.com>
2021-05-25 01:04:25 +0900

    Create 1920 수 찾기.py

---
+++ Baekjoon/이분 검색/1920 수 찾기.py
@@ -0,0 +1,26 @@
+n = int(input())
+arr1 = list(map(int, input().split()))
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+arr1.sort()
+
+for i in range(m):
+    left = 0
+    right = len(arr1)-1
+    result = -1
+    while(left <= right):
+        mid = int((left+right)/2)
+        
+        if arr1[mid] > arr2[i]:
+            right = mid - 1
+        elif arr1[mid] < arr2[i]:
+            left = mid + 1
+        else:
+            result = mid;
+            break
+    
+    if result != -1:
+        print(1)
+    else:
+        print(0)
