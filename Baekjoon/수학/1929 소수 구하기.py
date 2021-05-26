fb8f2bf06ed33edad7aa10e230799b76f4f8f1ad
kimuihyeon <57362030+kimuihyeon222@users.noreply.github.com>
2021-05-26 20:08:03 +0900

    Update 1929 소수 구하기.py

--- Baekjoon/수학/1929 소수 구하기.py
+++ Baekjoon/수학/1929 소수 구하기.py
@@ -4,6 +4,8 @@ import math
 n,m = map(int, sys.stdin.readline().split())
 
 for i in range(n, m+1):
+    if i == 1:
+        continue
     # 소수 판단
     check = 1
     for j in range(2, int(math.sqrt(i)+1)):
@@ -12,3 +14,5 @@ for i in range(n, m+1):
             break
     if check == 1:
         print(i)
+        
+# 1 소수 아님 ...
