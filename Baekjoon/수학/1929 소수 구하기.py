1198e97f0f34d920a95512e9a3ff636ae53b87f9
kimuihyeon <57362030+kimuihyeon222@users.noreply.github.com>
2021-05-26 19:59:35 +0900

    Create 1929 소수 구하기.py

---
+++ Baekjoon/수학/1929 소수 구하기.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+n,m = map(int, sys.stdin.readline().split())
+
+for i in range(n, m+1):
+    # 소수 판단
+    check = 1
+    for j in range(2, int(math.sqrt(i)+1)):
+        if i % j == 0:
+            check = -1
+            break
+    if check == 1:
+        print(i)
